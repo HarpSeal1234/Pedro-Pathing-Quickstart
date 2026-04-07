69d08ddb41389820c82320f3e3129178dedc4209
HarpSeal1234 <blueguh2010@gmail.com>
2026-04-06 21:32:43 -0400

    started red close

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CONSTANTS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CONSTANTS.java
@@ -33,9 +33,9 @@ public class CONSTANTS {
 
     // Maximum turret rotation in degrees (left or right from center)
     public static final double MAX_TURRET_ANGLE = 135;
-    public static final double TURRET_POSITION_PER_DEGREE =0.0016798245614035089;
+    public static final double TURRET_POSITION_PER_DEGREE = 0.00171/*0.0016798245614035089*/;
 
-    public  static  final Pose BLUE_NEAR_TELE_START = new Pose();
+    public  static  final Pose BLUE_NEAR_TELE_START = /*new Pose(15.5, 112.5, Math.toRadians(180))*/ new Pose(72,72, Math. toRadians(90));
     public  static  final Pose RED_NEAR_TELE_START = new Pose(108, 70, Math. toRadians(0));
     public  static  final Pose BLUE_FAR_TELE_START = new Pose();
     public static final Pose RED_FAR_TELE_START = new Pose();
